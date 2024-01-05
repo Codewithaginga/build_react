b0976e91739a63a7d39f3f62c48b1ebc5b0d8920
aginga <brianaginga35@gmail.com>
2024-01-05 15:55:04 +0300

    first push

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import Article from './components/article/Article'
+import Cta from './components/cta/Cta'
+import Brand from './components/brand/Brand'
+import Feature from './components/feature/Feature'
+import Navbar from './components/navbar/Navbar'
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
